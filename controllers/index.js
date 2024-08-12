9e411c352079d5c0ed1b8e4c569b58fc747a56df
unknown <brian.zhen101@gmail.com>
2024-08-12 12:39:21 -0400

    a lot of bug fixes and added code

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,10 @@ const router = require("express").Router();
 // Import all of the routes from controllers here
 const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api/");
-const pageOneRoutes = require("./pageOneRoutes");
+// localhost:3001/
 
 // Connect the routes to the router here
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/pageOne", pageOneRoutes);
 
 module.exports = router;
--- public/js/signup.js
+++ public/js/signup.js
@@ -7,12 +7,16 @@ const signupFormHandler = async function (event) {
   const passwordEl = document
     .querySelector("#password-input-signup")
     .value.trim();
+  const emailE1 = document
+    .querySelector("#email-input-signup")
+    .value.trim();
 
   if (passwordEl.length >= 8 && usernameEl) {
     const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({
         username: usernameEl,
+        email: emailE1,
         password: passwordEl,
       }),
       headers: { "Content-Type": "application/json" },
@@ -21,11 +25,11 @@ const signupFormHandler = async function (event) {
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to sign up");
+      alert("Failed to sign up. Please check your console log");
     }
   } else {
     alert(
-      "Please include both a username and password, and make sure your password is at least 8 characters long"
+      "Please include an username, email, and password, and make sure your password is at least 8 characters long Password must contain at least one uppercase letter Password must contain at least one lowercase letter Password must contain at least one number Password must contain at least one special character Password cannot contain username"
     );
   }
 };
